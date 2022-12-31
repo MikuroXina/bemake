acfa809a418f36db33e5c542ef4b9b1705b7ce3b
MikuroXina <ryosukadnak@gmail.com>
2022-12-31 14:09:36 +0900

    Customize theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Editor } from "./editor.jsx";
 import { StrictMode } from "react";
+import { extendTheme } from "@chakra-ui/react";
 import styles from "./index.module.css";
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+            100: "#e6ade6",
+            900: "#191d19",
+        },
+    },
+    styles: {
+        global: {
+            body: {
+                bg: "gray.700",
+                color: "#d0c9d0",
+            },
+        },
+    },
+});
 
 export const index = () => {
     return (
         <StrictMode>
-            <ChakraProvider>
+            <ChakraProvider theme={theme}>
                 <div className={styles.container}>
                     <Editor />
                 </div>
