9dd50a5adee8718535e846c59a4b241e02da1d10
MikuroXina <ryosukadnak@gmail.com>
2022-12-31 22:25:46 +0900

    Add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{AboutMetadata, CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +13,46 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .menu(make_menu())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn make_menu() -> Menu {
+    Menu::new()
+        .add_submenu(Submenu::new(
+            "File",
+            Menu::new()
+                .add_native_item(MenuItem::About("Bemake".into(), AboutMetadata::default()))
+                .add_item(CustomMenuItem::new(
+                    "checkForUpdates",
+                    "Check for Updates...",
+                ))
+                .add_native_item(MenuItem::Separator)
+                .add_item(CustomMenuItem::new("newBMS", "New BMS"))
+                .add_native_item(MenuItem::Separator)
+                .add_item(CustomMenuItem::new("openBMS", "Open BMS..."))
+                .add_native_item(MenuItem::Separator)
+                .add_item(CustomMenuItem::new("save", "Save"))
+                .add_item(CustomMenuItem::new("saveAs", "Save As..."))
+                .add_native_item(MenuItem::Separator)
+                .add_native_item(MenuItem::CloseWindow)
+                .add_native_item(MenuItem::Separator)
+                .add_native_item(MenuItem::Quit),
+        ))
+        .add_submenu(Submenu::new(
+            "Edit",
+            Menu::new()
+                .add_native_item(MenuItem::Undo)
+                .add_native_item(MenuItem::Redo)
+                .add_native_item(MenuItem::Separator)
+                .add_native_item(MenuItem::Cut)
+                .add_native_item(MenuItem::Copy)
+                .add_native_item(MenuItem::Paste),
+        ))
+        .add_submenu(Submenu::new(
+            "Help",
+            Menu::new().add_item(CustomMenuItem::new("github", "Open the GitHub repository")),
+        ))
 }
