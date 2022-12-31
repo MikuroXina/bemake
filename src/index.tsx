b8bd4290e5f70b08b64bf9208fef57bd1165427a
MikuroXina <ryosukadnak@gmail.com>
2022-12-31 12:22:53 +0900

    Setup project

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { invoke } from "@tauri-apps/api";
+import styles from "./index.module.css";
+
+export const index = () => {
+  const onGreet = async () => {
+    const response = await invoke("greet", { name: "World" });
+    console.log(response);
+  };
+
+  return (
+    <StrictMode>
+      <ChakraProvider>
+        <div className={styles.container}>
+          <h1>Hello, World!</h1>
+          <button onClick={onGreet}>Greet</button>
+        </div>
+      </ChakraProvider>
+    </StrictMode>
+  );
+};
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { index } from "./index.jsx";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("#root element not found");
+}
+createRoot(rootElement).render(index());
