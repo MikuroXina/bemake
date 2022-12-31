8f2f6d21fd6555c5b471d085c48c0790e1d77817
MikuroXina <ryosukadnak@gmail.com>
2022-12-31 16:24:45 +0900

    Implement scroll

--- src/editor/preview.tsx
+++ src/editor/preview.tsx
@@ -1,3 +1,5 @@
+import { WheelEvent, useState } from "react";
+
 import styles from "./preview.module.css";
 
 export interface Note {
@@ -19,15 +21,25 @@ const LANE_WIDTH = 40;
 const LANE_COLOR = "gray";
 const NOTE_HEIGHT = 10;
 const NOTE_SPEED = 80;
+const SCROLL_SPEED = 0.01;
 
 export const Preview = ({ notes, colorMap }: PreviewProps) => {
+    const [currentTime, setCurrentTime] = useState(0);
+
+    const onWheel = (e: WheelEvent) => {
+        if (e.deltaY === 0) {
+            return;
+        }
+        setCurrentTime((t) => Math.max(0, t - e.deltaY * SCROLL_SPEED));
+    };
+
     const noteElements = notes.map(({ id, time, lane }) => (
         <div
             key={`${lane}${time}${id}`}
             className={styles.note}
             style={{
                 left: lane * LANE_WIDTH,
-                bottom: time * NOTE_SPEED,
+                bottom: (time - currentTime) * NOTE_SPEED,
                 width: LANE_WIDTH,
                 height: NOTE_HEIGHT,
                 backgroundColor: colorMap[lane] || LANE_COLOR,
@@ -35,5 +47,9 @@ export const Preview = ({ notes, colorMap }: PreviewProps) => {
         ></div>
     ));
 
-    return <div className={styles.preview}>{noteElements}</div>;
+    return (
+        <div className={styles.preview} onWheel={onWheel}>
+            {noteElements}
+        </div>
+    );
 };
