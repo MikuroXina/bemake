6a7ac1e0538b97c0abf945d015f32f8a2f4c15ab
MikuroXina <ryosukadnak@gmail.com>
2023-03-23 01:19:22 +0900

    Implement stateFromChart

--- src/convert.ts
+++ src/convert.ts
@@ -1,20 +1,20 @@
-import { Note, NoteId, NoteLane, State } from "./editor/notes-context.js";
+import type { Note, NoteId, State } from "./editor/notes-context.js";
 
-const LANE_BY_CHANNEL: Record<string, NoteLane | undefined> = {
-    "11": "Key1",
-    "12": "Key2",
-    "13": "Key3",
-    "14": "Key4",
-    "15": "Key5",
-    "16": "Scratch",
-    "17": "FreeZone",
-    "18": "Key6",
-    "19": "Key7",
-};
-
-export interface Chart {}
+import type { Chart } from "./chart.js";
+import { NoteTimeCalculator } from "./note-time.js";
 
 export const stateFromChart = (chart: Chart): State => {
+    const calc = new NoteTimeCalculator(chart.notes);
     const notes: Record<NoteId, Note> = {};
+    let id = 0;
+    for (const note of chart.notes.objs) {
+        notes[`${id}` as NoteId] = {
+            objId: note.obj,
+            lane: note.key,
+            time: calc.seconds_by_time(note.offset),
+            selected: false,
+        };
+        id += 1;
+    }
     return { notes };
 };
